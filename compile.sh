e80c3890fcfe1d7b25f572276a5572abea94ebfc
Hunter Wilcox <hunter.wilcox@judgeglass.net>
2025-03-27 08:43:27 -0500

    not needed

--- compile.sh
+++
@@ -1,4 +0,0 @@
-ghdl -a and_gate.vhdl and_gate_tb.vhdl # Analyze source
-ghdl -e and_gate_tb # Build executable
-ghdl -r and_gate_tb --vcd=and_gate.vcd # Run simulation
-gtkwave and_gate.vcd # View waveform in GTKWave
